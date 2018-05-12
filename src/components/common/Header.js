f0275c37ed26a1dbf85b967d5bf2999f1f1f0504
D1o0g9s <boodalina@gmail.com>
2018-05-11 19:18:28 -0700

    implemented a couple things in Header.js and changed favicon.ico

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,19 +1,38 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
+import logo from '../../assets/logo.png';
+import lever from '../../assets/lever.png';
+import '../../styles/Header.css';
+import { Button } from 'reactstrap';
+
 
 const Header = ({loading}) => {
+
   return (
     <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
+      <span className="left logo">
+      <IndexLink to="/" activeClassName="active"><img src={logo} className="image" alt="IHAP Logo" /></IndexLink>
+      </span>
+
+      <span className="center"> 
       <Link to="/tickets" activeClassName="active">Tickets</Link>
       {" | "}
       <Link to="/profile" activeClassName="active">Profile</Link>
-      {" | "}
-      <Link to="/signin" activeClassName="active">Signin</Link>
-      {" | "}
-      <Link to="/signup" activeClassName="active">Signup</Link>
-      {" | "}
+        <span>
+        <img src={lever} className="image" alt="Lever" />
+        <img src={lever} className="upsidedownImage" alt="Lever" />
+        </span>
+        <span id="lever-options">
+          Random
+          Search
+        </span>
+      </span>
+      
+      <span className="right user">
+        <Link to="/signin" activeClassName="active"><Button className="button" color="primary" size="sm">Log in</Button></Link>
+        <br/ >
+        <Link to="/signup" activeClassName="active"><Button className="button" color="primary" size="sm">Sign up</Button></Link>
+      </span>
     </nav>
   );
 };
