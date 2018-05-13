13a31bb2dc9352ac881b4f1426f27c10eebe6c69
D1o0g9s <boodalina@gmail.com>
2018-05-12 17:08:38 -0700

    added stuff to nav bar

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,35 +3,84 @@ import { Link, IndexLink } from 'react-router';
 import logo from '../../assets/logo.png';
 import lever from '../../assets/lever.png';
 import '../../styles/Header.css';
-import { Button } from 'reactstrap';
+import { Button, Col, Input } from 'reactstrap';
 
 
-const Header = ({ loading }) => {
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      random: false,
+      rotation: 0,
+      loggedIn: false
+    }
+    this.handleLeverClick = this.handleLeverClick.bind(this);
+    this.setRandom = this.setRandom.bind(this);
+    this.setSearch = this.setSearch.bind(this);
+  }
 
-  return (
-    <nav id='nav'>
-      <div className="logo">
-        <IndexLink to="/" activeClassName="active"><img src={logo} className="image" alt="IHAP Logo" /></IndexLink>
+  handleLeverClick() {
+    if(this.state.random == true) {
+      this.setSearch();
+    } else {
+      this.setRandom();
+    }
+  }
+
+  setRandom() {
+    this.setState(
+      {
+        random: true, 
+        rotation: 180, 
+      }
+    );
+  } 
+
+  setSearch() {
+    this.setState(
+      {
+        random: false, 
+        rotation: 0, 
+      }
+    );
+  }
+
+
+  render() {
+    const searchOrButton = this.state.random ? (
+      <Button color="primary">GET RANDOM TICKET</Button>
+    ) : (
+      <div> 
+        <Input type="search" name="search" placeholder="Search" />
+        <Button color="primary">SEARCH</Button>
       </div>
-
-      <div className="center">
-        <div>
-          <img src={lever} className="image" alt="Lever" />
-          <img src={lever} className="upsidedownImage" alt="Lever" />
-        </div>
-        <div>
-          <p>Random</p>
-          <p>Search</p>
-        </div>
-      </div>
-
-      <div className="user">
-        <Link to="/signin" activeClassName="active"><Button className="button" color="primary" size="sm">Log in</Button></Link>
-        <br />
-        <Link to="/signup" activeClassName="active"><Button className="button" color="primary" size="sm">Sign up</Button></Link>
-      </div>
-    </nav>
-  );
-};
-
-export default Header;
+    ); 
+    return (
+      <nav id='nav'>
+        <Col className="logo" xs="3">
+          <IndexLink to="/" activeClassName="active"><img src={logo} className="image" alt="IHAP Logo" /></IndexLink>
+        </Col>
+  
+        <Col className="center" xs="auto">
+          {searchOrButton}
+          <div id="lever">
+            <div>
+              <img src={lever} alt="Lever" className="image clickable" style={{transform: `rotate(${this.state.rotation}deg)`}} onClick={this.handleLeverClick} />
+            </div>
+            <div>
+              <div className="clickable" id="random" onClick={this.setRandom}>Random</div>
+              <div className="clickable" id="search" onClick={this.setSearch}>Search</div>
+            </div>
+          </div>
+        </Col>
+  
+        <Col className="user" xs="3">
+          <Link to="/signin" activeClassName="active"><Button className="button" color="primary" size="sm">Log in</Button></Link>
+          <br />
+          <Link to="/signup" activeClassName="active"><Button className="button" color="primary" size="sm">Sign up</Button></Link>
+        </Col>
+      </nav>
+    );
+  }
+  
+}
