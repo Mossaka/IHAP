193f6bc34cb29977bac3cb5bf1303ef3feab0131
Sui Gao <Mark_sVi@outlook.com>
2018-05-20 16:41:48 -0700

    remove unused

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root'));
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'));
 registerServiceWorker();
