5178a5ad0944355942f61d02521467f43eff419e
Sui Gao <Mark_sVi@outlook.com>
2018-05-20 14:09:49 -0700

    problem & solution editor

--- src/components/App.js
+++ src/components/App.js
@@ -8,21 +8,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './home/HomePage';
 import ProfilePage from "./profile/ProfilePage";
 import ViewTicket from "./ticket/ViewTicket";
+import Editor from './ticket/Editor';
 
 class App extends Component {
   render() {
     return (
-      <div className="App"> 
-        <header className="App-header">
-          <Header/>
-        </header>
-        <Route path="/signup" component={Signup}/>
-        <Route path="/signin" component={Signin}/>
-        <Switch>
-          <Route path="/profile" component={ProfilePage}/>
-          <Route path="/tickets" component={ViewTicket}/>
-          <Route component={HomePage}/>
-        </Switch>
+      <div className="container">
+        <div className="row">
+          <div className="col-5">
+            <Editor>
+            </Editor>
+          </div>
+        </div>
       </div>
     );
   }
