56a791c533c6499814d49197d93a82792c1e977c
Sui Gao <Mark_sVi@outlook.com>
2018-05-20 21:53:37 -0700

    hook up ticket page

--- src/ticket/TicketPage.js
+++ src/ticket/TicketPage.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import Content from './Content';
+import RelateTicket from './RelatedTicket';
+import { Container, Row, Col } from 'reactstrap';
 
 export default class TicketPage extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
   }
 
   render() {
     return (
-      <div className="container">
-
-      </div>
+      <Container>
+        <Row>
+          <Col xs="6">
+            <h3>Problem</h3>
+            <hr />
+            <Content />
+            <div className="mt-5">
+              <RelateTicket />
+            </div>
+          </Col>
+          <Col xs="6">
+            <h3>Solutions</h3>
+            <hr />
+            <Content />
+            <Content />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
