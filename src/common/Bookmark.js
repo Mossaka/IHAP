623746b0b10fa3af475166449d637fb7d4db9715
Mossaka <jiz417@ucsd.edu>
2018-05-21 23:19:54 -0700

    finished bookmarking

--- src/common/Bookmark.js
+++ src/common/Bookmark.js
@@ -7,7 +7,26 @@ class Bookmark extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            bookmarked: false
+        }
         this.onBookmarkClick = this.onBookmarkClick.bind(this);
+
+        firebase.auth().onAuthStateChanged(user => {
+            if(user) {
+                const uid = user.uid;
+                const ticketID = this.props.ticketID;
+                firebase.database().ref('notebooks/' + uid + '/bookmarked').on('value', snapshot => {
+                    snapshot.forEach(childSnapshot => {
+                        let bookmarked = childSnapshot.val() === this.props.ticketID;
+                        console.log(bookmarked)
+                        this.setState({bookmarked: bookmarked});
+                    })
+                })
+            } else {
+                this.setState({bookmarked: false})
+            }
+        })
     }
 
     onBookmarkClick(event) {
@@ -26,9 +45,10 @@ class Bookmark extends React.Component {
                 })
                 if(count != null) {
                     updates['bookmarkCount'] = count
+                    this.setState({bookmarked: true})
                     firebase.database().ref('tickets/' + ticketID).update(updates)
+                    firebase.database().ref('notebooks/' + uid + '/bookmarked/').push(ticketID);
                 }
-                firebase.database().ref('notebooks/' + uid + '/bookmarked/').push(ticketID);
             } else {
                 alert("user not signin!");
             }
@@ -38,13 +58,23 @@ class Bookmark extends React.Component {
     }
 
     render(){
-        return (
-            <div className='bookmark'>
-                <svg width='16' height='42' style={{float:'right'}} onClick={this.onBookmarkClick}>
-                    <polygon points='0,0 0,33 8,40 16,33 16,0' />
-                </svg>
-            </div>
-        )
+        if(!this.state.bookmarked) {
+            return (
+                <div className='bookmark'>
+                    <svg width='16' height='42' style={{float:'right'}} onClick={this.onBookmarkClick}>
+                        <polygon points='0,0 0,33 8,40 16,33 16,0' />
+                    </svg>
+                </div>
+            )
+        } else {
+            return (
+                <div className='bookmark'>
+                    <svg width='16' height='42' style={{float:'right'}}>
+                        <polygon style={{opacity: '1'}} points='0,0 0,33 8,40 16,33 16,0' />
+                    </svg>
+                </div>
+            )
+        }
     }
 
 }
