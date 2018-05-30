3f9ba1ad7eb956ff0e74111d53d83cf7951ae26d
Sui Gao <Mark_sVi@outlook.com>
2018-05-29 20:15:36 -0700

    support unbookmark

--- src/common/Bookmark.js
+++ src/common/Bookmark.js
@@ -1,84 +1,62 @@
 import firebase from 'firebase';
 import React from 'react';
-import './Bookmark.css'
+import './Bookmark.css';
 
-class Bookmark extends React.Component {
-    constructor(props) {
-        super(props)
+export default class Bookmark extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            bookmarked: false
-        }
-        this.onBookmarkClick = this.onBookmarkClick.bind(this);
+    this.state = {
+      disabled: true,
+      bookmarked: false
+    };
 
-        firebase.auth().onAuthStateChanged(user => {
-            if(user) {
-                const uid = user.uid;
-                firebase.database().ref('notebooks/' + uid + '/bookmarked').on('value', snapshot => {
-                    snapshot.forEach(childSnapshot => {
-                        if( childSnapshot.val() === this.props.ticketID ) {
-                            this.setState({bookmarked: true});
-                        }
-                    })
-                })
-            } else {
-                this.setState({bookmarked: false})
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ disabled: false });
+        firebase.database()
+          .ref('notebooks/' + user.uid + '/bookmarked')
+          .orderByValue()
+          .equalTo(props.ticketID).once('value', ss => {
+            if (ss.val()) {
+              for (let key in ss.val()) {
+                this.key = key;
+              }
+              this.setState({ bookmarked: true });
             }
-        })
+          });
+      } else {
+        this.setState({ disabled: true });
+      }
+    });
+  }
+
+  bookmark = e => {
+    if (this.state.disabled) {
+      alert('Please sign in to bookmark this ticket');
+      return;
     }
 
-    onBookmarkClick(event) {
-        firebase.auth().onAuthStateChanged(user => {
-            if(user) {
-                // var problemID = null;
-                const uid = user.uid;
-                const ticketID = this.props.ticketID;
-                // firebase.database().ref('tickets/' + ticketID + "/problem").on('value', (snapshot) => {
-                //     problemID = snapshot.val();  
-                // })
-                if(this.state.bookmarked === false) {
-                    this.setState({bookmarked: true})
-                    firebase.database().ref('notebooks/' + uid + '/bookmarked/').push(ticketID);
-                } 
-            } else {
-                alert("user not signin!");
-            }
-        })
-        
-        event.preventDefault();
+    let uid = firebase.auth().currentUser.uid;
+    let db = firebase.database();
+    if (this.state.bookmarked) {
+      console.log(this.key)
+      db.ref('notebooks/' + uid + '/bookmarked/' + this.key).remove();
+      this.setState({ bookmarked: false });
     }
-
-    render(){
-        if(!this.state.bookmarked) {
-            return (
-                <div className='bookmark'>
-                <svg width='25' height='40' style={{float:'right'}} onClick={this.onBookmarkClick}>
-                    <g>
-                        <path d="M12 17.27l6.18 3.73-1.64-7.03 5.46-4.73-7.19-.61-2.81-6.63-2.81 6.63-7.19.61 5.46 4.73-1.64 7.03z"></path>
-                    </g>
-                </svg>    
-  
-                    {/*<svg width='16' height='42' style={{float:'right'}} onClick={this.onBookmarkClick}>
-                        <polygon points='0,0 0,33 8,40 16,33 16,0' />
-                    </svg>*/}
-                </div>
-            )
-        } else {
-            return (
-                <div className='bookmark'>
-                    <svg width='25' height='40' style={{float:'right'}}>
-                    <g style={{opacity: '1'}}>
-                        <path d="M12 17.27l6.18 3.73-1.64-7.03 5.46-4.73-7.19-.61-2.81-6.63-2.81 6.63-7.19.61 5.46 4.73-1.64 7.03z"></path>
-                    </g>
-                    </svg> 
-                    {/*<svg width='16' height='42' style={{float:'right'}}>
-                        <polygon style={{opacity: '1'}} points='0,0 0,33 8,40 16,33 16,0' />
-                    </svg>*/}
-                </div>
-            )
-        }
+    else {
+      this.key = db.ref('notebooks/' + uid + '/bookmarked').push(this.props.ticketID).key;
+      this.setState({ bookmarked: true });
     }
+  }
 
+  render() {
+    return (
+      <svg className='bookmark' onClick={this.bookmark}>
+        <g className={`${this.state.disabled ? 'disabled' : ''}${this.state.bookmarked ? ' bookmarked' : ''}`}>
+          <path d="M12 17.27l6.18 3.73-1.64-7.03 5.46-4.73-7.19-.61-2.81-6.63-2.81 6.63-7.19.61 5.46 4.73-1.64 7.03z" />
+        </g>
+      </svg>
+    );
+  }
 }
-
-export default Bookmark;
