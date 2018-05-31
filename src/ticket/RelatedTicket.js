4598239ed41de42045c02dcb05b13074ca68ace2
Kevin Huang <origamijr@sbcglobal.net>
2018-05-30 21:51:05 -0700

    Fixed related tickets

--- src/ticket/RelatedTicket.js
+++ src/ticket/RelatedTicket.js
@@ -38,28 +38,38 @@ export default class RelatedTicket extends React.Component {
     firebase.database().ref('tickets/' + this.props.id).once('value').then(t => {
       var title = t.val().title.toLowerCase();
       var content = stripHtml(t.val().content).toLowerCase();
+      var keyword = title + " " + content;
+      for (var i = 0; i < content.length / 10; i++) {
+        keyword += " " + title;
+      }
 
-      weightedSearch(title + " " + content, 4, {title: 3, content: 2}).then(function(ids) {
+      weightedSearch(keyword, 4, {title: 3, content: 1}).then(function(ids) {
         
-        //self.setState({related1: <CardLink href={ids[1]}>{self.state.title1}</CardLink>});
-        //self.setState({related2: <CardLink href={ids[2]}>{self.state.title2}</CardLink>});
-        //self.setState({related3: <CardLink href={ids[3]}>{self.state.title3}</CardLink>});
-        //self.setTitles(ids);
+        if(ids.length > 1) { 
+          firebase.database().ref('tickets/' + ids[1]).once('value').then(t => {
+            self.setState({related1: <CardLink href={ids[1]}>{t.val().title}</CardLink>});
+          });
 
-        firebase.database().ref('tickets/' + ids[1]).once('value').then(t => {
-          self.setState({title1: t.val().title});
-          self.setState({related1: <CardLink href={ids[1]}>{t.val().title}</CardLink>});
-        });
-
-        firebase.database().ref('tickets/' + ids[2]).once('value').then(t => {
-          self.setState({title2: t.val().title});
-          self.setState({related2: <CardLink href={ids[2]}>{t.val().title}</CardLink>});
-        });
-
-        firebase.database().ref('tickets/' + ids[3]).once('value').then(t => {
-          self.setState({title3: t.val().title});
-          self.setState({related3: <CardLink href={ids[3]}>{t.val().title}</CardLink>});
-        });
+          if (ids.length > 2) {
+            firebase.database().ref('tickets/' + ids[2]).once('value').then(t => {
+              self.setState({related2: <CardLink href={ids[2]}>{t.val().title}</CardLink>});
+            });
+            if (ids.length > 3) {
+              firebase.database().ref('tickets/' + ids[3]).once('value').then(t => {
+                self.setState({related3: <CardLink href={ids[3]}>{t.val().title}</CardLink>});
+              });
+            } else {
+              self.setState({related3: <p></p>});
+            }
+          } else {
+            self.setState({related2: <p></p>});
+            self.setState({related3: <p></p>});
+          }
+        } else {
+          self.setState({related1: <p>No Related Tickets Found</p>});
+          self.setState({related2: <p></p>});
+          self.setState({related3: <p></p>});
+        }
       });
     });
   }
