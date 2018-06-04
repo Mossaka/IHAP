9c26c56ce192484a505bb7b5776dc63a9f67c977
Sui Gao <Mark_sVi@outlook.com>
2018-06-03 22:35:30 -0700

    comment ui

--- src/common/TimeDisplay.js
+++ src/common/TimeDisplay.js
@@ -3,5 +3,7 @@ import './TimeDisplay.css'
 
 export default function TimeDisplay(props) {
   let d = new Date(Number(props.time));
-  return <span className='timeDisplay'>{'Last Edit: '+d.toLocaleString()}</span>;
+  let prefix = 'Last Edit: ';
+  if (props.prefix) prefix = props.prefix;
+  return <span className='timeDisplay'>{prefix + d.toLocaleString()}</span>;
 }
--- src/ticket/Solution.js
+++ src/ticket/Solution.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import firebase from 'firebase';
-import { Card, CardBody, CardText,FormGroup,Label,Input,Button, Row, Col,CardTitle,CardSubtitle } from 'reactstrap';
+import { Card, CardBody, CardText, FormGroup, Label, Input, Button, Row, Col, CardTitle, CardSubtitle } from 'reactstrap';
 import TimeDisplay from '../common/TimeDisplay';
 import Vote from './Vote';
 import Avatar from '../common/Avatar';
@@ -16,14 +16,14 @@ export default class Solution extends React.Component {
     this.state = {
       edit: false,
       loaded: false,
-      showAnswerForm : false,
+      showAnswerForm: false,
       comment: '',
       comments: [],
       name: ''
     };
   }
 
-  handleSubmit = () =>{
+  handleSubmit = () => {
     let now = new Date().getTime();
     let userId = firebase.auth().currentUser.uid;
     let newData = {
@@ -32,30 +32,30 @@ export default class Solution extends React.Component {
       dateEdited: now
     }
     let y = firebase.database().ref('comments').push(newData).getKey();
-    firebase.database().ref('solutions/'+this.props.id+'/comments').push(y);
+    firebase.database().ref('solutions/' + this.props.id + '/comments').push(y);
     this.handleCloseModal();
     this.refresh();
-    this.setState({comment : ''})
+    this.setState({ comment: '' })
   }
-  handleCommentChange(e){
-    this.setState({comment: e.target.value});
+  handleCommentChange(e) {
+    this.setState({ comment: e.target.value });
   }
   componentDidMount() {
     let init = [];
     firebase.database().ref('solutions/' + this.props.id).once('value', s => {
       this.setState({ ...s.val(), loaded: true });
       let comments = s.val().comments;
-      for(let comment in comments){
+      for (let comment in comments) {
         firebase.database().ref('comments/' + comments[comment]).once('value', s => {
           let name = s.val().creator;
-          firebase.database().ref("profiles/"+name + "/username").once('value', t => {
+          firebase.database().ref("profiles/" + name + "/username").once('value', t => {
             var data = {
               content: s.val().content,
               creator: t.val(),
-              dateEdited:s.val().dateEdited
+              dateEdited: s.val().dateEdited
             }
             init.push(data);
-            this.setState ({comments : init});
+            this.setState({ comments: init });
           });
         });
       }
@@ -63,22 +63,22 @@ export default class Solution extends React.Component {
   }
 
 
-  refresh(){
-        let init = [];
+  refresh() {
+    let init = [];
     firebase.database().ref('solutions/' + this.props.id).once('value', s => {
       this.setState({ ...s.val(), loaded: true });
       let comments = s.val().comments;
-      for(let comment in comments){
+      for (let comment in comments) {
         firebase.database().ref('comments/' + comments[comment]).once('value', s => {
           let name = s.val().creator;
-          firebase.database().ref("profiles/"+name + "/username").once('value', t => {
+          firebase.database().ref("profiles/" + name + "/username").once('value', t => {
             var data = {
               content: s.val().content,
               creator: t.val(),
-              dateEdited:s.val().dateEdited
+              dateEdited: s.val().dateEdited
             }
             init.push(data);
-            this.setState ({comments : init});
+            this.setState({ comments: init });
           });
         });
       }
@@ -87,26 +87,27 @@ export default class Solution extends React.Component {
   toggleEditor = () => {
     this.setState({ edit: !this.state.edit });
   }
-  handleOpenModal = () =>{
-    this.setState({showAnswerForm : true});
+  handleOpenModal = () => {
+    this.setState({ showAnswerForm: true });
     console.log(this.state.comments);
   }
-  handleCloseModal =  () => {
+  handleCloseModal = () => {
     this.setState({ showAnswerForm: false });
   }
 
   render() {
-    const Test = ({comments}) => (
-      <div>
-        {Object.keys(comments).map(comment => (
-          <Card>
-            <CardTitle> User: {comments[comment].creator }</CardTitle>
-            <CardSubtitle>Comments : { comments[comment].content }</CardSubtitle>
-            <TimeDisplay time={comments[comment].dateEdited} />
-          </Card>
+    const Test = ({ comments }) => (
+      <div className="comments">
+        {Object.keys(comments).map((comment, idx) => (
+          <div key={idx}>
+            <span className="name">{comments[comment].creator}</span>
+            <TimeDisplay time={comments[comment].dateEdited} prefix="commented on: " />
+            <p>{comments[comment].content}</p>
+          </div>
         ))}
       </div>
     );
+
     if (!this.state.loaded) {
       return <h1>Loading...</h1>;
     }
@@ -131,22 +132,23 @@ export default class Solution extends React.Component {
           <CardText dangerouslySetInnerHTML={{ __html: this.state.content }} />
           <TimeDisplay time={this.state.dateEdited} />
         </CardBody>
-        <div className="clearfix" style = {{ display: 'inline-block' }}>
-          <div className = "float-left"> <Vote up={this.state.upvote} down={this.state.downvote} path={'solutions/' + this.props.id} ></Vote></div>
-          <p className="float-right" onClick = { this.handleOpenModal } >comment</p>
+        <div className="clearfix bottom" style={{ display: 'inline-block' }}>
+          <div className="float-left"> <Vote up={this.state.upvote} down={this.state.downvote} path={'solutions/' + this.props.id} ></Vote></div>
+          <Button className="float-right" color="info" onClick={this.handleOpenModal} size="sm">comment</Button>
         </div>
         <ReactModal isOpen={this.state.showAnswerForm}>
           <Avatar id={this.state.creator} isAnonymous={false} />
-          <p dangerouslySetInnerHTML={{ __html: this.state.content }}/>
+          <p dangerouslySetInnerHTML={{ __html: this.state.content }} />
           <FormGroup>
             <Label >Comment</Label>
-            <Input type="textarea" value={this.state.comment} onChange={evt => this.handleCommentChange(evt) }/>
+            <Input type="textarea" value={this.state.comment} onChange={evt => this.handleCommentChange(evt)} />
           </FormGroup>
           <div>
-            <Button className="float-left" color="danger" onClick = {this.handleCloseModal}> Close </Button>
-            <Button className="float-right" color="primary" onClick = { this.handleSubmit } > Submit </Button>
+            <Button className="float-left" color="danger" onClick={this.handleCloseModal}> Close </Button>
+            <Button className="float-right" color="primary" onClick={this.handleSubmit} > Submit </Button>
           </div>
         </ReactModal>
+        <hr />
         <Test comments={this.state.comments} > </Test>
       </Card>
     );
